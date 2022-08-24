6e7a1052ea00989c1feef9f288944dcd5fc7ddfa
Matthieu Mota <matthieumota@gmail.com>
2022-08-24 11:36:16 +0200

    Prepare book model

---
+++ src/Model/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Book\Mvc\Model;
+
+class Book extends Model
+{
+    protected $id;
+    protected $title;
+    protected $price;
+    protected $isbn;
+    protected $author;
+    protected $published_at;
+    protected $image;
+}
